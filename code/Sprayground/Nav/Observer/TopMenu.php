680f5be49bd68a83130a50112ec29e503e81d39c
Alex Lukyanau <cybergomel@gmail.com>
2017-06-10 00:46:12 -0400

    Nav Menu

---
+++ code/Sprayground/Nav/Observer/TopMenu.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sprayground\Nav\Observer;
+
+use  Magento\Framework\Event\ObserverInterface;
+
+class TopMenu implements ObserverInterface
+{
+    public function __construct()
+    {
+        //Observer initialization code...
+        //You can use dependency injection to get any class this observer may need.
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $menu=$observer->getMenu();
+        $transportObject=$observer->getData('transportObject');
+        $html=$transportObject->getHtml();
+        $transportObject->setHtml('Vodka!');
+        //Observer execution code...
+    }
+}
